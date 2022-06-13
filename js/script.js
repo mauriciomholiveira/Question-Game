abca5a39399e7d310265870b0620cc89b990a650
Mauricio Oliveira <mauriciomholiveira@icloud.com>
2022-06-13 19:08:51 -0300

    projeto final 1.0

--- js/script.js
+++ js/script.js
@@ -40,10 +40,11 @@ const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
 
 console.log(respostas[numeroAleatorio])
 elementoResposta.innerHTML = pergunta + respostas [numeroAleatorio]
+elementoResposta.style.opacity = 1;
 
 // sumir a resposta depois de 3 segundos
  setTimeout(function() {
-   elementoResposta.style.opacity = 1;
+   elementoResposta.style.opacity = 0;
    buttonPerguntar.removeAttribute('disabled')
  }, 3000)
 
